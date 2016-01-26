f2b47f3df6e97514f9186f7429f838f9a6f58114
Wulf Solter <wulf@wulf.co.nz>
2016-01-26 21:16:12 +1300

    index.ts compiled as umd module

--- index.js
+++ index.js
@@ -1,45 +1,46 @@
-System.register([], function(exports_1) {
+(function (factory) {
+    if (typeof module === 'object' && typeof module.exports === 'object') {
+        var v = factory(require, exports); if (v !== undefined) module.exports = v;
+    }
+    else if (typeof define === 'function' && define.amd) {
+        define(["require", "exports"], factory);
+    }
+})(function (require, exports) {
     /*jshint bitwise: false*/
     "use strict";
-    var UUID;
-    return {
-        setters:[],
-        execute: function() {
-            UUID = (function () {
-                function UUID() {
-                    // no-op
-                }
-                UUID.UUID = function () {
-                    if (typeof (window.crypto) !== "undefined" && typeof (window.crypto.getRandomValues) !== "undefined") {
-                        // If we have a cryptographically secure PRNG, use that
-                        // http://stackoverflow.com/questions/6906916/collisions-when-generating-uuids-in-javascript
-                        var buf = new Uint16Array(8);
-                        window.crypto.getRandomValues(buf);
-                        var S4 = function (num) {
-                            var ret = num.toString(16);
-                            while (ret.length < 4) {
-                                ret = "0" + ret;
-                            }
-                            return ret;
-                        };
-                        return (S4(buf[0]) + S4(buf[1]) + "-" + S4(buf[2]) + "-" + S4(buf[3]) + "-" + S4(buf[4]) + "-" + S4(buf[5]) + S4(buf[6]) + S4(buf[7]));
-                    }
-                    else {
-                        // Otherwise, just use Math.random
-                        // https://stackoverflow.com/questions/105034/create-guid-uuid-in-javascript
-                        // https://stackoverflow.com/questions/11605068/why-does-jshint-argue-against-bitwise-operators-how-should-i-express-this-code
-                        function s4() {
-                            return Math.floor((1 + Math.random()) * 0x10000)
-                                .toString(16)
-                                .substring(1);
-                        }
-                        return s4() + s4() + "-" + s4() + "-" + s4() + "-" +
-                            s4() + "-" + s4() + s4() + s4();
-                    }
-                };
-                return UUID;
-            })();
-            exports_1("UUID", UUID);
+    var UUID = (function () {
+        function UUID() {
+            // no-op
         }
-    }
+        UUID.UUID = function () {
+            if (typeof (window.crypto) !== "undefined" && typeof (window.crypto.getRandomValues) !== "undefined") {
+                // If we have a cryptographically secure PRNG, use that
+                // http://stackoverflow.com/questions/6906916/collisions-when-generating-uuids-in-javascript
+                var buf = new Uint16Array(8);
+                window.crypto.getRandomValues(buf);
+                var S4 = function (num) {
+                    var ret = num.toString(16);
+                    while (ret.length < 4) {
+                        ret = "0" + ret;
+                    }
+                    return ret;
+                };
+                return (S4(buf[0]) + S4(buf[1]) + "-" + S4(buf[2]) + "-" + S4(buf[3]) + "-" + S4(buf[4]) + "-" + S4(buf[5]) + S4(buf[6]) + S4(buf[7]));
+            }
+            else {
+                // Otherwise, just use Math.random
+                // https://stackoverflow.com/questions/105034/create-guid-uuid-in-javascript
+                // https://stackoverflow.com/questions/11605068/why-does-jshint-argue-against-bitwise-operators-how-should-i-express-this-code
+                function s4() {
+                    return Math.floor((1 + Math.random()) * 0x10000)
+                        .toString(16)
+                        .substring(1);
+                }
+                return s4() + s4() + "-" + s4() + "-" + s4() + "-" +
+                    s4() + "-" + s4() + s4() + s4();
+            }
+        };
+        return UUID;
+    })();
+    exports.UUID = UUID;
 });
